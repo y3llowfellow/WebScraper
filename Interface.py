e60dd830488258acff5fa4b36c71c4325f94e8cd
y3llowfellow <119467521+y3llowfellow@users.noreply.github.com>
2024-02-25 12:41:47 -0600

    Shifted table function from Interface.py to PriceTracker.py, adding email function to Scraper objects.

--- Interface.py
+++ Interface.py
@@ -1,67 +1,56 @@
 from PriceTracker import Scraper
-from urllib.request import urlopen
+import warnings
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
+
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, render_template_string
 import threading
 from datetime import datetime
 import numpy as np
 import pandas as pd
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 
-watchlist = [
-    "https://www.amazon.com/Apple-Generation-Cancelling-Transparency-Personalized/dp/B0CHWRXH8B/ref=sr_1_4?crid=2328EFCIMKH5J&keywords=airpods&qid=1706060481&sprefix=airpo%2Caps%2C152&sr=8-4&ufe=app_do%3Aamzn1.fos.f5122f16-c3e8-4386-bf32-63e904010ad0",
-    "https://www.amazon.com/Mighty-Patch-Hydrocolloid-Absorbing-count/dp/B074PVTPBW/?_encoding=UTF8&pd_rd_w=uYSSW&content-id=amzn1.sym.64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_p=64be5821-f651-4b0b-8dd3-4f9b884f10e5&pf_rd_r=WHK1T4SQ8QGKJ0K6TFVX&pd_rd_wg=g2948&pd_rd_r=a0263006-aa76-45f1-bf1e-b0b4528ee417&ref_=pd_gw_crs_zg_bs_3760911"
-]
-sc = Scraper(watchlist[1])
 
-name = sc.getTitle()
-print(sc.getPrice())
+#prompt users
+number = int(input("How many items would you like to track?"))
+scrapers = []
 
-# html_template = """
-# <!DOCTYPE html>
-# <html lang = "en">
-#     <head>
-#         <meta charset="UTF-8" />
-#         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-#         <h1>Price Tracker</h1>
-#     </head>
-#     <body>
-#
-#     </body>
-# </html>
-# """
-
-# beginning of Html file
-html_head = """
-<!DOCTYPE html>
-<html lang = "en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <h1>Price Tracker</h1>
-    </head>
-    <body>
-"""
-
-# end of html file
-html_end = """
-    </body>
-</html>
-"""
-
-#create table for to display prices and time
-w, h = 2, 0
-prices = [[0 for x in range(w)] for y in range(h)]
-prices.append(["Times", "Prices"])
+#populate watchlist with user entered links
+for i in range(1, number+1):
+    link = input("Enter item " + str(i) + " url: " )
+    sc = Scraper(link)
+    scrapers.append(sc)
 
 #populate table w/ prices
 def updateTables():
-    #threading.Timer(15, updateTables).start()
-    prices.append([datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), sc.getPrice()])
-    df = pd.DataFrame(prices)
-    htmlChart = df.to_html(header=False, index=False)
-    ProductName = "<h2>" + name + "<h2>\n"
-    finalStr = (html_head + ProductName + htmlChart + html_end)
-    return finalStr
+    # beginning of Html file template
+    html_head = """
+    <!DOCTYPE html>
+    <html lang = "en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <h1>Price Tracker</h1>
+        </head>
+        <body>
+    """
+    # end of html file template
+    html_end = """
+        </body>
+    </html>
+    """
+
+    for j in range(0, number):
+        # create table for to display prices and time
+        table = scrapers[j].getTable()
+        df = pd.DataFrame(table)
+        htmlChart = df.to_html(header=False, index=False)
+        ProductName = "<h2>" + scrapers[j].getTitle() + "<h2>\n"
+        html_head += ProductName + htmlChart
+    return html_head + html_end
+
 
 
 app = Flask(__name__)
--- PriceTracker.py
+++ PriceTracker.py
@@ -1,14 +1,20 @@
+import smtplib
+from datetime import datetime
 from urllib.request import urlopen
 from selenium import webdriver
 from bs4 import BeautifulSoup
-
+from smtplib import SMTP
 
 class Scraper:
     def __init__(self, link):
-        self.price = None
+        self.price = []
+        self.time = []
+
+        self.table = [[0 for x in range(2)] for y in range(0)]
+        self.table.append(["Times", "Prices"])
         self.title = None
         self.link = link
-        self.populate()
+
 
     def populate(self):
         page = urlopen(self.link)
@@ -16,13 +22,28 @@ class Scraper:
         html = html_bytes.decode("utf8")
         soup = BeautifulSoup(html, "html.parser")
         # image = soup.find('div',{'id':'imageBlock'}).text.strip()
-        self.title, self.price = soup.find('h1', {'id': 'title'}).text.strip(), soup.find('span', {
-            'class': 'a-offscreen'}).text.strip()
 
-    def getPrice(self):
+        lastPrice = self.table[len(self.table)-1][1]
+        time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        newPrice = soup.find('span', {
+            'class': 'a-offscreen'}).text.strip()
+        self.table.append([time, newPrice])
+
+        self.title = soup.find('h1', {'id': 'title'}).text.strip()
+
+        if(lastPrice == newPrice):
+            message_text = self.getMail(lastPrice, newPrice)
+            with smtplib.SMTP("smtp.server.address", 587) as smtp:
+                smtp.starttls()
+                smtp.login("pricealerts611@gmail.com", "meowmeow1#")
+                smtp.sendmail("pricealerts611@gmail.com", "colin.yx.shen@gmail.com", message_text)
+
+    def getTable(self):
         self.populate()
-        return self.price
+        return self.table
 
     def getTitle(self):
-        self.populate()
         return self.title
+
+    def sendMail(self, oldPrice, newPrice):
+        return "Price has changed from " + oldPrice + " to " + newPrice
