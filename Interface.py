611c8dcb2bd343666b2c99f14828c33a291f03be
y3llowfellow <119467521+y3llowfellow@users.noreply.github.com>
2024-02-25 20:09:26 -0600

    Added fail-safe for glitchy timer

--- Interface.py
+++ Interface.py
@@ -1,3 +1,5 @@
+import datetime
+
 from PriceTracker import Scraper
 import warnings
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
@@ -6,11 +8,12 @@ from flask import Flask, render_template, render_template_string
 import pandas as pd
 
 
+
 #prompt users
 number = int(input("How many items would you like to track?"))
 scrapers = []
 email = input("what is your email address?")
-
+RefreshRate = input("How often would you like to update the price table? Enter time in ms: ")
 #populate watchlist with user entered links
 for i in range(1, number+1):
     link = input("Enter item " + str(i) + " url: " )
@@ -22,6 +25,8 @@ finalHtml = ""
 
 app = Flask(__name__)
 
+lastTime = datetime.datetime(2024, 2, 20, 11, 11, 11)
+previous = " "
 #populate table w/ prices
 @app.route('/data')
 def updateTables():
@@ -42,7 +47,7 @@ def updateTables():
                     $('*').html(data);  // update page with new data
                   });
                 };
-                setInterval(updater, 1000);  // run `updater()` every 1000ms (1s)
+                setInterval(updater,""" + RefreshRate + """);  // run `updater()` every 1000ms (1s)
             </script>
     """
     # end of html file template
@@ -50,15 +55,21 @@ def updateTables():
         </body>
     </html>
     """
+    global lastTime
+    if (datetime.datetime.now() - lastTime).total_seconds() > (int(RefreshRate)/1000-1):
+        lastTime = datetime.datetime.now()
+        print("hi")
+        for j in range(0, number):
+            # create table for to display prices and time
+            table = scrapers[j].getTable()
+            df = pd.DataFrame(table)
+            htmlChart = df.to_html(header=False, index=False)
+            ProductName = "<h2>" + scrapers[j].getTitle() + "<h2>\n"
+            html_head += ProductName + htmlChart
+            global previous
+            previous = html_head + html_end
+    return previous
 
-    for j in range(0, number):
-        # create table for to display prices and time
-        table = scrapers[j].getTable()
-        df = pd.DataFrame(table)
-        htmlChart = df.to_html(header=False, index=False)
-        ProductName = "<h2>" + scrapers[j].getTitle() + "<h2>\n"
-        html_head += ProductName + htmlChart
-    return html_head + html_end
 @app.route('/')
 def home():
     return render_template_string(updateTables())
