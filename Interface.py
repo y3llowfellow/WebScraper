dde09239c730879893426e8db67878daa1b78df2
y3llowfellow <119467521+y3llowfellow@users.noreply.github.com>
2024-02-25 21:23:29 -0600

    Finalized and added comments!

--- Interface.py
+++ Interface.py
@@ -5,13 +5,12 @@ warnings.simplefilter(action='ignore', category=DeprecationWarning)
 from flask import Flask, render_template, render_template_string
 import pandas as pd
 import logging
-#disble unnecessary console logs w/ logging import
+#Disble unnecessary console logs w/ logging import
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 import argparse
 
-
-#prompt users using commandline
+#Prompt users using the command line
 parser = argparse.ArgumentParser(description='Track items and send emails')
 parser.add_argument('num_items', type=int, help='Number of items to track')
 parser.add_argument('interval_ms', type=int, help='Milliseconds between checks')
@@ -22,26 +21,24 @@ number = args.num_items
 scrapers = []
 email = args.email
 RefreshRate = args.interval_ms
-#populate watchlist with user entered links
+#Populate watchlist with user entered links
 for i in range(1, number+1):
     link = input("Enter item " + str(i) + " url: " )
     sc = Scraper(link, email)
     scrapers.append(sc)
-
-
-#begin flask webpage
+#Begin flask webpage
 app = Flask(__name__)
 
-#record the last time the table was updated to ensure that the timer is working properly
+#Record the last time the table was updated to ensure that the timer is working properly
 lastTime = datetime.datetime(2024, 2, 20, 11, 11, 11)
 
-#record the last updated table, in case if updateTables() prematurely updates
+#Record the last updated table, in case if updateTables() prematurely updates
 previous = " "
 
-#populate table w/ prices
+#Populate table w/ prices
 @app.route('/data')
 def updateTables():
-    # beginning of Html file template
+    # Beginning of HTML file template
     html_head = """
     <!DOCTYPE html>
     <html lang = "en">
@@ -61,37 +58,34 @@ def updateTables():
                 setInterval(updater,""" + str(RefreshRate) + """);  // run `updater()` every 1000ms (1s)
             </script>
     """
-    # end of html file template
+    #End of html file template
     html_end = """
         </body>
     </html>
     """
     global lastTime
-    #check if table needs to be updated based on refresh rate
+    #Check if table needs to be updated based on the refresh rate
     if (datetime.datetime.now() - lastTime).total_seconds() > (int(RefreshRate)/1000-1):
         lastTime = datetime.datetime.now()
-        #loop through each product
+        #Loop through each product
         for j in range(0, number):
             # create table for to display prices and time using scraper objects
             table = scrapers[j].getTable()
             df = pd.DataFrame(table)
             htmlChart = df.to_html(header=False, index=False)
-
-            #add product name as a title
+            #Add product name as a title
             ProductName = "<h2>" + scrapers[j].getTitle() + "<h2>\n"
-
-            #combine all html files together
+            #Combine all HTML files together
             html_head += ProductName + htmlChart
             global previous
             previous = html_head + html_end
     return previous
-
-#render flask webpage with html file
+#Render Flask webpage with HTML file from the updateTables() function
 @app.route('/')
 def home():
     return render_template_string(updateTables())
-
-#launch flask website
+#Launches Flask website
 if __name__ == '__main__':
+    #Prints website for user access
     print("Go to http://127.0.0.1:5000/ on Google for live updates!")
     app.run()
