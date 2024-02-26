0b264394f1f5bd0219e0e64f955b1030ca9c74cd
y3llowfellow <119467521+y3llowfellow@users.noreply.github.com>
2024-02-25 20:37:51 -0600

    Added Email Alert Feature

--- PriceTracker.py
+++ PriceTracker.py
@@ -5,8 +5,9 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from smtplib import SMTP
 
+
 class Scraper:
-    def __init__(self, link):
+    def __init__(self, link, email):
         self.price = []
         self.time = []
 
@@ -14,7 +15,7 @@ class Scraper:
         self.table.append(["Times", "Prices"])
         self.title = None
         self.link = link
-
+        self.email = email
 
     def populate(self):
         page = urlopen(self.link)
@@ -23,7 +24,7 @@ class Scraper:
         soup = BeautifulSoup(html, "html.parser")
         # image = soup.find('div',{'id':'imageBlock'}).text.strip()
 
-        lastPrice = self.table[len(self.table)-1][1]
+        lastPrice = self.table[len(self.table) - 1][1]
         time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
         newPrice = soup.find('span', {
             'class': 'a-offscreen'}).text.strip()
@@ -31,12 +32,8 @@ class Scraper:
 
         self.title = soup.find('h1', {'id': 'title'}).text.strip()
 
-        if(lastPrice == newPrice):
-            message_text = self.getMail(lastPrice, newPrice)
-            with smtplib.SMTP("smtp.server.address", 587) as smtp:
-                smtp.starttls()
-                smtp.login("pricealerts611@gmail.com", "meowmeow1#")
-                smtp.sendmail("pricealerts611@gmail.com", "colin.yx.shen@gmail.com", message_text)
+        if (lastPrice != newPrice):
+            self.sendMail(lastPrice, newPrice)
 
     def getTable(self):
         self.populate()
@@ -46,4 +43,7 @@ class Scraper:
         return self.title
 
     def sendMail(self, oldPrice, newPrice):
-        return "Price has changed from " + oldPrice + " to " + newPrice
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.login("pricealerts611@gmail.com", "atju krdm dwwl mqsf")
+        server.sendmail("pricealerts611@gmail.com", self.email,
+                        "The price of " + self.title +" has changed from " + str(oldPrice) + " to " + str(newPrice))
